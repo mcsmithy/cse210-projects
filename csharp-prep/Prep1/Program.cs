cca98f2931067602a0861bf666d3f45cbe386962
Benton McNabb <benton@mcnabbs.org>
2026-09-19 23:26:12 -0400

    Finished first project

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,11 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.Write("What is your first name? ");
+        string first = Console.ReadLine();
+        Console.Write("What is your last name? ");
+        string second = Console.ReadLine();
+        Console.WriteLine();
+        Console.WriteLine($"Your name is {second}, {first} {second}.");
     }
 }
