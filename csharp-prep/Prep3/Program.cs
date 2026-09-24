42a5f23881b0dd3e353ed569e14ea230b43e4b55
Benton McNabb <benton@mcnabbs.org>
2026-09-24 09:32:56 -0400

    Finished program

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,42 @@
 using System;
+using System.Reflection.Emit;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        //setup
+        //Console.Write("What is the magic number? ");
+        //string input = Console.ReadLine();
+        //int number = int.Parse(input);
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1, 101);
+
+        int guess = 0;
+        int score = 0;
+
+        //game
+        Console.WriteLine("Welcome to the number guessing game!");
+        while (guess != number)
+        {
+                
+            Console.Write("What is your guess? ");
+            string guessInput = Console.ReadLine();
+            guess = int.Parse(guessInput);
+            score = score + 1;
+            if (guess > number)
+            {
+                Console.WriteLine("Lower");
+            }
+            else if (guess < number)
+            {
+                Console.WriteLine("Higher");
+            }
+            else
+            {
+                Console.WriteLine($"Congrats! You guessed the number {number}!");
+            }
+        }
+        Console.WriteLine($"It took you {score} guesses!");
     }
 }
